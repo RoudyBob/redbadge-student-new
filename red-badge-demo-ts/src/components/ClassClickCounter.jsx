45e18220bf683421acd4bb8cdb808293110fe7d6
Bob Roudebush <bob@roudybob.net>
2021-03-25 19:33:43 -0400

    click counter with state

--- red-badge-demo-ts/src/components/ClassClickCounter.jsx
+++ red-badge-demo-ts/src/components/ClassClickCounter.jsx
@@ -1,1 +1,29 @@
-// Create a class version of the Functional Component
+import React, { Component } from 'react';
+import { Button, Container } from "reactstrap";
+
+class ClassClickCounter extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {countValue: this.props.initialValue};
+        // this.handleClick = this.handleClick.bind(this);
+    };
+
+    handleClick = () => {
+        if (this.state.countValue > 0) {
+            let tempCount = this.state.countValue - 1;
+            this.setState({countValue: tempCount});
+        };
+    };
+
+    render () {
+        return (
+            <Container>
+            <p>The total clicks left are {this.state.countValue}</p>
+            <Button onClick={this.handleClick}> Click Me From Function</Button>
+          </Container>
+        );
+    };
+};
+
+export default ClassClickCounter;
